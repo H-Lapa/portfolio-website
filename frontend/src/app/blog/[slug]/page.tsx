46856b3ec42e36e2b3245aa2757d5b2ced7c5345
Hugo Lapa <Hugolapa888@gmail.com>
2025-12-21 12:47:52 +0000

    initial commit

---
+++ frontend/src/app/blog/[slug]/page.tsx
@@ -0,0 +1,11 @@
+
+export default function Home() {
+    return (
+      <div className="">
+  
+        <h1>This is the template for any future blog posts</h1>
+       
+      </div>
+    );
+  }
+  
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className="">
+
+      <h1>Main Page</h1>
+
+      <img src="" alt="Picture of Hugos Face" />
+     
+      <p>Summary of Hugo</p>
+
+      <hr />
+
+      <h2>Recent Blogs</h2>
+
+      <p> list of different posts</p>
+
+      <h2>Pinned Projects</h2>
+
+      <p>Gallary of projects</p>
+    </div>
+  );
+}
